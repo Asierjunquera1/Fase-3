627dc698ebf29507007ef840c647f278e9a3ec79
Asierjunquera1 <ajunquerabartolome@gmail.com>
2023-05-26 12:16:22 +0200

    aaaaa

--- routes/contactos.py
+++ routes/contactos.py
@@ -1,15 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+
 
 
 contactos= Blueprint("contactos", __name__)
 
-@contactos.route("/")
-def home():
+@contactos.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return redirect(url_for('enviar'))
+    
     return render_template("index.html")
 
-@contactos.route("/formulario")
-def add_contact():
-    return render_template("formulario.html")
+    
+
+@contactos.route("/enviar")
+def enviado():
+    return render_template("formulario_enviado.html")
+
+
+
 
 
 
